33414c88bd9a2a9365a35eb923b0171e28a65897
Eduardo Camacho <eduardocc97@hotmail.com>
2020-05-31 23:57:27 -0600

    Se cambio la conexion a una clase que se encargara de hacer las consultas y ahora se traen los datos de la bd y se regresa el id del usuario logeado

--- projecBlue/Controllers/AccountController.cs
+++ projecBlue/Controllers/AccountController.cs
@@ -10,8 +10,6 @@ namespace projecBlue.Controllers
 {
     public class AccountController : Controller
     {
-        SqlConnection con = new SqlConnection("data source=DESKTOP-3J5RQC4\\SQLEXPRESS; database=pruebas; integrated security=SSPI;");
-        SqlDataReader dr;
         // GET: Account
         [HttpGet]
         public ActionResult Login()
@@ -21,21 +19,15 @@ namespace projecBlue.Controllers
         [HttpPost]
         public ActionResult Verify(Account acc)
         {
-
-            con.Open();
-            SqlCommand com = new SqlCommand("SELECT * FROM tblLogin WHERE usuario=@usuario AND pass=@pass", con);
-            com.Parameters.AddWithValue("@usuario", acc.Name);
-            com.Parameters.AddWithValue("@pass", acc.Password);
-            dr = com.ExecuteReader();
-            if (dr.Read())
+            clsConsultas consulta = new clsConsultas();
+            int idLogeado = consulta.Login(acc.Name, acc.Password);
+            if (idLogeado == 0)
             {
-                con.Close();
-                return View("Create");
+                return View("Error");
             }
             else
             {
-                con.Close();
-                return View("Error");
+                return View("Create");
             }
         }
     }
--- projecBlue/Models/clsConsultas.cs
+++ projecBlue/Models/clsConsultas.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using projecBlue.Models;
+using System.Data;
 
 namespace projecBlue.Models
 {
     public class clsConsultas
     {
-
+        SqlConnection con = new SqlConnection("data source=DESKTOP-3J5RQC4\\SQLEXPRESS; database=pruebas; integrated security=SSPI;");
+        public int Login(string user, string pass)
+        {
+            try
+            {
+                SqlDataAdapter com = new SqlDataAdapter("SELECT * FROM tblLogin WHERE usuario=@usuario AND pass=@pass", con);
+                com.SelectCommand.Parameters.AddWithValue("@usuario", user);
+                com.SelectCommand.Parameters.AddWithValue("@pass", pass);
+                DataTable dt = new DataTable();
+                com.Fill(dt);
+                con.Close();
+                int cantidad = dt.Rows.Count;
+                if (cantidad != 0)
+                {
+                    return Convert.ToInt32(dt.Rows[0]["id"]); //Regresamos el ID del usuario logeado
+                }
+                else
+                {
+                    return 0; // si no se encontro el usuario o hubo un error
+                }
+            }
+            catch
+            {
+                return 0; // si no se encontro el usuario o hubo un error
+            }
+        }
     }
 }
